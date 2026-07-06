88a823d585992204c78f5e31a68585af4a9e1779
Alex <alex-fjp@hotmail.com>
2026-07-05 22:25:13 -0300

    ✨ feat: Adiciona rotas para gerenciamento de pessoas com operações de cadastro, listagem e exclusão

--- controlegastos.api/Program.cs
+++ controlegastos.api/Program.cs
@@ -1,5 +1,6 @@
 using controlegastos.api.Data;  
 using Microsoft.EntityFrameworkCore;
+using controlegastos.api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,5 +13,39 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// CONFIGURANDO ROTAS DE PESSOAS.
+
+// Async serve para que a aplicação não fique bloqueada enquanto aguarda a resposta do banco de dados, permitindo que outras requisições sejam processadas simultaneamente.
+// Await serve para que a aplicação aguarde a conclusão da operação assíncrona antes de prosseguir, garantindo que os dados sejam salvos corretamente no banco de dados.
+// Operações Assíncronas evita que a aplicação não fique bloqueada enquanto aguarda a resposta do banco de dados, permitindo que outras requisições sejam processadas simultaneamente.
+
+// Cadastrar pessoa(POST)
+app.MapPost("/pessoas", async (AppDbContext db, Pessoa pessoa) =>
+{
+    db.Pessoas.Add(pessoa); // Adiciona a pessoa ao banco de dados.
+    await db.SaveChangesAsync(); // Salva as alterações no banco de dados.
+    return Results.Created($"/pessoas/{pessoa.Id}", pessoa); // Retorna o status 201 Created com a URL da nova pessoa criada.
+});
+
+// Listar pessoa(GET)
+app.MapGet("/pessoas", async (AppDbContext db) =>
+{
+    var pessoas = await db.Pessoas.ToListAsync(); // Mostra todas as pessoas cadastradas no banco de dados.
+    return Results.Ok(pessoas); // Retorna o status 200 OK com a lista de pessoas.  
+});
+
+// Deletar pessoa(DELETE)
+app.MapDelete("/pessoas/{id}", async (AppDbContext db, int id) =>
+{
+    var pessoa = await db.Pessoas.FindAsync(id); // FindAsyncencontra a pessoa pelo ID. Ele retorna a entidade encontrada ou null se não houver correspondência.
+    
+    if (pessoa is null) // Se a pessoa não for encontrada pelo ID.
+        return Results.NotFound("Pessoa não encontrada."); // Retorna o status 404 Not Found.
+
+    db.Pessoas.Remove(pessoa); // Remove a pessoa do banco de dados.
+    await db.SaveChangesAsync(); // Salva as alterações no banco de dados.
+    return Results.Ok("Pessoa e suas transações foram excluídas com sucesso."); // Retorna o status 200 OK com a mensagem de sucesso.
+});
 
 app.Run();
