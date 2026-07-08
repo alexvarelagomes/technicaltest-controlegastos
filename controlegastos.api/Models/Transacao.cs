442f664b2ec37097ffd55412e77919ea00b82a7c
Alex <alex-fjp@hotmail.com>
2026-07-08 20:11:52 -0300

    ✨ feat: Adiciona anotação [JsonIgnore] na propriedade Pessoa da classe Transacao para evitar serialização em JSON

--- controlegastos.api/Models/Transacao.cs
+++ controlegastos.api/Models/Transacao.cs
@@ -1,5 +1,6 @@
-namespace controlegastos.api.Models
+using System.Text.Json.Serialization;
 
+namespace controlegastos.api.Models
 { public class Transacao
     { 
         public int Id { get; set; }
@@ -9,6 +10,8 @@ namespace controlegastos.api.Models
 
         // Relacionamento com a tabela Pessoa (Chave estrangeira)
         public int PessoaId { get; set; } // Chave estrangeira para a tabela
-        public Pessoa? Pessoa { get; set; }
+
+        [JsonIgnore] // Evita que a propriedade Pessoa seja serializada em JSON, prevenindo problemas de referência circular ao retornar dados da API.
+        public Pessoa? Pessoa { get; set; } // Propriedade de navegação para a entidade Pessoa, permitindo acessar os dados da pessoa relacionada à transação.
     }
 }
