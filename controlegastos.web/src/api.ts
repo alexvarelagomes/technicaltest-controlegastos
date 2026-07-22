0fc30e459b6db4e13948fe8677519104259597eb
Alex <alex-fjp@hotmail.com>
2026-07-21 21:42:26 -0300

    fix: Atualiza a URL da API para o ambiente de produção

--- controlegastos.web/src/api.ts
+++ controlegastos.web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 // Cria uma instância com a URL da API.
-const api = axios.create({baseURL: 'http://localhost:5055',});
+const api = axios.create({baseURL: 'https://api-controle-gastos-si2n.onrender.com/',});
 
 export default api;
